c19c33df921afe7da6d2a0d7c3ab5783b82bd3a4
Oskar Duveborn <oskar@flowersfx.com>
2017-01-07 13:29:05 +0100

    initial commit to vso

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Mandagsklubben.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var lessBundle = new Bundle("~/Styles").IncludeDirectory("~/Content", "*.less");
+            lessBundle.Transforms.Add(new LessTransform());
+            lessBundle.Transforms.Add(new CssMinify());
+            bundles.Add(lessBundle);
+
+            bundles.Add(new ScriptBundle("~/JavaScripts").Include(
+                        "~/Scripts/moment.min.js",
+                        "~/Scripts/knockout-2.3.0.js",
+                         "~/Scripts/jquery-2.0.3.min.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+
+        private class LessTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                response.Content = dotless.Core.Less.Parse(response.Content);
+                response.ContentType = "text/css";
+            }
+        }
+    }
+}
---
+++ Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mandagsklubben.Web.MvcApplication" Language="C#" %>
